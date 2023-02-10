2d4e82d833ab537588ddc7ef7fc282b75e6b9a8c
Dhananjay-12 <pantpriyanshu31@gmail.com>
2023-02-11 00:57:22 +0530

    ejs files set up and basic behavior added to application

--- server.js
+++ server.js
@@ -1,11 +1,36 @@
-const express = require("express")
+const express = require('express');
+const morgan = require('morgan');
+const bodyParser = require('body-parser');
+const path = require('path');
+require('dotenv').config();
+const app = express();
 
-const app = express()
+const port = process.env.PORT || 4000;
+
+//logging requests
+
+app.use(morgan('tiny'));
+
+//parsing request with body parser
+
+app.use(bodyParser.urlencoded({ extended: true }));
+
+//setting view engine
+app.set('view engine', 'ejs');
+
+//Loading assests
+app.use('/css', express.static(path.join(__dirname, 'assets/css')));
+app.use('/img', express.static(path.join(__dirname, 'assets/img')));
+app.use('/js', express.static(path.join(__dirname, 'assets/js')));
+
+//base route
 
 app.get('/', (req, res) => {
-    res.send("Hello There...")
-})
+  res.render('index');
+});
+
+//Listening to port
 
 app.listen(port, () => {
-    console.log(`Server started at : http//localhost:${port}`)
-})
+  console.log(`Server started at : http//localhost:${port}`);
+});
