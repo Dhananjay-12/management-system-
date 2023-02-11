35fce30eaadd4e43fc5e8acd486e981e81d9061c
Dhananjay-12 <pantpriyanshu31@gmail.com>
2023-02-11 19:46:08 +0530

    router set

--- server/routes/router.js
+++ server/routes/router.js
@@ -1,9 +1,15 @@
 const express = require('express');
 const route = express.Router();
 const { homeRoutes, addUser, updateUser } = require('./../services/render');
-
+const controller = require('./../controller/controller');
 route.get('/', homeRoutes);
 route.get('/add-user', addUser);
 route.get('/update-user', updateUser);
+
+//API handling
+route.post('/api/users', controller.create);
+route.get('/api/users', controller.find);
+route.put('/api/users/:id', controller.update);
+route.delete('/api/users/:id', controller.delete);
 
 module.exports = route;
